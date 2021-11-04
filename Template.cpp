2e9baa2f60721de9b38c0530c97e08d221f7fd4e
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2021-11-04 10:31:26 +0530

    updated template and added debug header

---
+++ Template.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#ifdef ONPC
+    #include </Users/tanmayraj/Documents/CP/Library/Debug.h>
+#else
+  #define deb(x...)
+#endif
+using namespace std;
+// =================
+
+const int MOD = 1e9+7;
+const int N = 3e5 + 5;
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int TEST = 1;
+    // cin >> TEST;
+    for (int i = 1; i <= TEST; i++) {
+        // cout << "Case #" << i << ": ";
+        solve();
+        #ifdef ONPC
+            cout << "__________________________" << endl;
+        #endif
+    }
+    #ifdef ONPC
+        cerr << endl << "finished in " << clock() * 1.0 / CLOCKS_PER_SEC << " sec" << endl;
+    #endif
+    return 0;
+}
