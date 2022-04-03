43501f63970f99e61395ac2992b3301e80b8b751
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2022-04-03 13:49:53 +0530

    remove sieve

--- Algorithms/factorise.cpp
+++ Algorithms/factorise.cpp
@@ -1,34 +1,16 @@
-const int NUM = 1e5+5;
-bool prime[NUM];
+vector<vector<array<int, 2>>> factors;
 
-void sieve() {
-    memset(prime,true,sizeof(prime));
-    prime[0]=false;
-    prime[1]=false;
-    for(int i = 2; i*i < NUM; ++i) {
-        if(prime[i]) { 
-            for(int j = i*i; j < NUM; j+=i) {
-                prime[j] = false;
+void factorise_all(int n) {
+    factors.resize(n);
+    for (int i = 2; i < n; i++) {
+        if (factors[i].empty() == false) continue;
+        for (int j = i; j < n; j += i) {
+            int val = j, cnt = 0;
+            while (val % i == 0) {
+                cnt++;
+                val /= i;
             }
+            if (cnt) factors[j].push_back({i, cnt});
         }
     }
-}
-
-vector<array<int, 2>> factorise(int x) {
-    if (prime[2] == false) sieve();
-    vector<array<int, 2>> factors;
-    for (int i = 2; i < NUM && x > 1; ++i) {
-        if (prime[i] == false) continue;
-        int cnt = 0;
-        while (x % i == 0) {
-            cnt++;
-            x /= i;
-        }
-        if (cnt > 0) factors.push_back({i, cnt});
-        if (prime[x]) {
-            factors.push_back({x, 1});
-            break;
-        }
-    }
-    return factors;
 }
